4b89f50f6d64ae32f2007b05bd4f46e9bc999c83
r-pluss <r.pluss52@gmail.com>
2016-11-17 14:17:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
 
+const debugMode = false;
+const batarangPath = '/User Data/Default/Extensions/ighdmehidhipcmcojjgiloacoafjmpfk/0.10.7_0'
+if(debugMode){
+    BrowserWindow.addDevToolsExtension(batarangPath);
+}
+
 app.on('ready', function(){
     mainWindow = new BrowserWindow({
         width: electron.screen.getPrimaryDisplay().size.width,
